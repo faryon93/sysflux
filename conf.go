00d4298ac7c0169e8a539df8e69f555d99b6c103
Maximilian Pachl <m@ximilian.info>
2018-06-22 22:06:35 +0200

    fix(conf): properly read "batch_size" property

--- conf.go
+++ conf.go
@@ -36,18 +36,18 @@ type Conf struct {
 }
 
 type ConfInflux struct {
-	Addr     string `yaml:"string"`
-	User     string `yaml:"user"`
-	Password string `yaml:"password"`
-	Database string `yaml:"database"`
+	Addr     string
+	User     string
+	Password string
+	Database string
 }
 
 type ConfSyslog struct {
-	Database    string `yaml:"database"`
-	Measurement string `yaml:"measurement"`
-	Listen      string `yaml:"listen"`
-	Regex       string `yaml:"regex"`
-	BatchSize   int    `yaml:"batch_size"`
+	Database    string
+	Measurement string
+	Listen      string
+	Regex       string
+	BatchSize   int    `mapstructure:"batch_size"`
 }
 
 // ---------------------------------------------------------------------------------------
